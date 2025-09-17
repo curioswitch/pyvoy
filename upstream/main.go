6e3f63818b4bca0ec9dec95741d9c3f50862344a
Choko <choko@curioswitch.org>
2025-09-17 11:24:41 +0900

    Cleanup upstream

--- upstream/main.go
+++ upstream/main.go
@@ -17,11 +17,8 @@ func main() {
 	srv := &http.Server{
 		Protocols: &prots,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			println("Got req")
 			w.WriteHeader(200)
 			c := http.NewResponseController(w)
-			c.EnableFullDuplex()
-			w.Write([]byte("a"))
 			c.Flush()
 			<-r.Context().Done()
 		}),
