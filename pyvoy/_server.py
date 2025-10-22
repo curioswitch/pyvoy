9b820999599027c4e6a4aea685bcd08a7d97b574
Choko <choko@curioswitch.org>
2025-10-22 16:03:53 +0900

    Fix TLS setup

--- pyvoy/_server.py
+++ pyvoy/_server.py
@@ -300,10 +300,7 @@ class PyvoyServer:
                                 "typed_config": {
                                     "@type": "type.googleapis.com/envoy.extensions.transport_sockets.quic.v3.QuicDownstreamTransport",
                                     "downstream_tls_context": {
-                                        "common_tls_context": common_tls_context,
-                                        "require_client_certificate": bool(
-                                            self._tls_ca_cert
-                                        ),
+                                        "common_tls_context": common_tls_context
                                     },
                                 },
                             },
