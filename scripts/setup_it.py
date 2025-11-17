33010d19adcf7ca8f7718d925d0ebd8023a9a8fb
Choko <choko@curioswitch.org>
2025-11-17 17:10:58 +0900

    Add integration tests with connect-python (#73)

---
+++ scripts/setup_it.py
@@ -0,0 +1,39 @@
+import shutil
+import subprocess
+import sys
+from io import BytesIO
+from pathlib import Path
+from zipfile import ZipFile
+
+import httpx
+
+
+def setup(name: str, repo: str, revision: str) -> None:
+    dest = Path(__file__).parent.parent / "out" / "integration" / name
+    dest_path = Path(dest)
+    # For now, don't try to be incremental since syncing Python version changes
+    # wouldn't be trivial.
+    if dest_path.exists():
+        shutil.rmtree(dest_path)
+
+    dest_path.mkdir(parents=True)
+
+    url = f"https://github.com/{repo}/archive/{revision}.zip"
+
+    response = httpx.get(url, follow_redirects=True)
+    response.raise_for_status()
+
+    with ZipFile(BytesIO(response.content)) as zf:
+        for member in zf.infolist():
+            # strip the top-level directory
+            filename = member.filename.split("/", 1)[1]
+            dest = dest_path / filename
+            if member.is_dir():
+                dest.mkdir(parents=True, exist_ok=True)
+            else:
+                dest.write_bytes(zf.read(member))
+
+    py = f"{sys.version_info.major}.{sys.version_info.minor}"
+    subprocess.run(
+        ["uv", "sync", "--python", py, "--directory", str(dest_path)], check=True
+    )
