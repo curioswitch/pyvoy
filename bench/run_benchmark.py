9ccd32e3f106a1939a79ea33e8ba1c59cec1eda5
Choko <choko@curioswitch.org>
2025-11-19 11:40:54 +0900

    Measure CPU/RAM in benchmark (#80)

--- bench/run_benchmark.py
+++ bench/run_benchmark.py
@@ -2,10 +2,13 @@ import argparse
 import json
 import subprocess
 import sys
+import threading
 import time
 import urllib.request
 from dataclasses import dataclass
 from enum import Enum
+
+import psutil
 
 
 @dataclass
@@ -31,6 +34,75 @@ class Protocol(Enum):
 
 class Args(argparse.Namespace):
     short: bool
+
+
+@dataclass
+class ResourceMeasurement:
+    cpu_percent: float
+    rss: int
+
+
+@dataclass
+class ResourceAggregate:
+    min: ResourceMeasurement
+    max: ResourceMeasurement
+    avg: ResourceMeasurement
+
+
+class ResourceMonitor:
+    def __init__(self, pid: int) -> None:
+        self._process = psutil.Process(pid)
+        self._started = False
+        self._measurements: list[ResourceMeasurement] = []
+        self._lock = threading.Lock()
+
+        self._started = False
+        self._thread: threading.Thread | None = None
+
+        # Initialize cpu percent measurement
+        self._record()
+
+    def start(self) -> None:
+        self._started = True
+        self._thread = threading.Thread(target=self._run, daemon=True)
+        self._thread.start()
+
+    def stop(self) -> None:
+        self._started = False
+        if self._thread is not None:
+            self._thread.join()
+            self._thread = None
+
+    def clear(self) -> None:
+        with self._lock:
+            self._measurements.clear()
+
+    def aggregate(self) -> ResourceAggregate:
+        with self._lock:
+            measurements = self._measurements.copy()
+        min_cpu = min(m.cpu_percent for m in measurements)
+        max_cpu = max(m.cpu_percent for m in measurements)
+        avg_cpu = sum(m.cpu_percent for m in measurements) / len(measurements)
+        min_rss = min(m.rss for m in measurements)
+        max_rss = max(m.rss for m in measurements)
+        avg_rss = sum(m.rss for m in measurements) / len(measurements)
+        return ResourceAggregate(
+            min=ResourceMeasurement(min_cpu, min_rss),
+            max=ResourceMeasurement(max_cpu, max_rss),
+            avg=ResourceMeasurement(avg_cpu, int(avg_rss)),
+        )
+
+    def _record(self) -> ResourceMeasurement:
+        with self._process.oneshot():
+            cpu_percent = self._process.cpu_percent(interval=None)
+            rss = self._process.memory_info().rss
+        return ResourceMeasurement(cpu_percent, rss)
+
+    def _run(self) -> None:
+        while self._started:
+            with self._lock:
+                self._measurements.append(self._record())
+            time.sleep(0.5)
 
 
 def main() -> None:
@@ -71,6 +143,17 @@ def main() -> None:
                 stdout, stderr = server.communicate()
                 msg = f"Server {app_server.name} failed to start\n{stderr.decode()}\n{stdout.decode()}"
                 raise RuntimeError(msg)
+
+            pid = server.pid
+            if app_server in (GRANIAN, HYPERCORN, PYVOY):
+                # find the worker process
+                parent = psutil.Process(server.pid)
+                children = parent.children()
+                pid = children[-1].pid
+
+            monitor = ResourceMonitor(pid)
+            monitor.start()
+
             for protocol in (Protocol.HTTP2, Protocol.HTTP1):
                 if protocol != Protocol.HTTP1 and app_server == UVICORN:
                     continue
@@ -102,15 +185,21 @@ def main() -> None:
                         ]
                         if protocol == Protocol.HTTP2:
                             vegeta_args.append("-h2c")
+
+                        monitor.clear()
                         vegeta = subprocess.Popen(  # noqa: S603
                             vegeta_args,
                             stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE,
                             stderr=sys.stderr,
                         )
+
                         vegeta_result, _ = vegeta.communicate(
                             f"{json.dumps(target)}\n".encode()
                         )
+
+                        resource = monitor.aggregate()
+
                         subprocess.run(
                             [  # noqa: S607
                                 "go",
@@ -121,7 +210,16 @@ def main() -> None:
                             check=True,
                             input=vegeta_result,
                         )
+
+                        print(  # noqa: T201
+                            f"\nCPU Percentage\t[Min, Max, Avg]\t\t{resource.min.cpu_percent:.2f}, {resource.max.cpu_percent:.2f}, {resource.avg.cpu_percent:.2f}%"
+                        )
+                        print(  # noqa: T201
+                            f"Memory RSS\t[Min, Max, Avg]\t\t{resource.min.rss}, {resource.max.rss}, {resource.avg.rss}\n"
+                        )
+
                         print("\n", flush=True)  # noqa: T201
+            monitor.stop()
             server.terminate()
             server.communicate()
 
