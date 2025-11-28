56f12ab7b8b521e50964768bbbe6969354fb84ae
Choko <choko@curioswitch.org>
2025-11-28 12:08:52 +0900

    Add wsgi to bench (#97)

--- bench/run_benchmark.py
+++ bench/run_benchmark.py
@@ -17,16 +17,27 @@ import psutil
 class AppServer:
     name: str
     args: list[str]
+    asgi_args: list[str] | None
+    wsgi_args: list[str] | None
 
 
-APP = "tests.apps.asgi.kitchensink:app"
+ASGI_APP = "tests.apps.asgi.kitchensink:app"
+WSGI_APP = "tests.apps.wsgi.kitchensink:app"
 
-PYVOY = AppServer("pyvoy", ["pyvoy", APP])
-HYPERCORN = AppServer("hypercorn", ["hypercorn", "--worker-class", "uvloop", APP])
-GRANIAN = AppServer(
-    "granian", ["granian", "--interface", "asgi", "--loop", "uvloop", APP]
+PYVOY = AppServer("pyvoy", ["pyvoy"], [ASGI_APP], ["--interface", "wsgi", WSGI_APP])
+HYPERCORN = AppServer(
+    "hypercorn", ["hypercorn", "--worker-class", "uvloop"], [ASGI_APP], [WSGI_APP]
 )
-UVICORN = AppServer("uvicorn", ["uvicorn", "--no-access-log", "--loop", "uvloop", APP])
+GRANIAN = AppServer(
+    "granian",
+    ["granian", "--loop", "uvloop"],
+    ["--interface", "asgi", ASGI_APP],
+    ["--interface", "wsgi", WSGI_APP],
+)
+GUNICORN = AppServer("gunicorn", ["gunicorn"], None, [WSGI_APP])
+UVICORN = AppServer(
+    "uvicorn", ["uvicorn", "--no-access-log", "--loop", "uvloop"], [ASGI_APP], None
+)
 
 
 class Protocol(Enum):
@@ -123,110 +134,123 @@ def main() -> None:
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
     )
-    for app_server in (PYVOY, HYPERCORN, GRANIAN, UVICORN):
-        if not sys._is_gil_enabled() and app_server == GRANIAN:  # noqa: SLF001
-            # Granian hangs on free-threaded for some reason
-            continue
-        with subprocess.Popen(  # noqa: S603
-            app_server.args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        ) as server:
-            # Wait for server to start
-            started = False
-            for _ in range(100):
-                try:
-                    with urllib.request.urlopen(
-                        "http://localhost:8000/controlled"
-                    ) as resp:
-                        if resp.status == 200:
-                            started = True
-                            break
-                except Exception:  # noqa: S110
-                    pass
-                time.sleep(0.1)
-            if server.returncode is not None or not started:
-                server.terminate()
-                stdout, stderr = server.communicate()
-                msg = f"Server {app_server.name} failed to start\n{stderr.decode()}\n{stdout.decode()}"
-                raise RuntimeError(msg)
 
-            pid = server.pid
-            if app_server in (GRANIAN, HYPERCORN, PYVOY):
-                # find the worker process
-                parent = psutil.Process(server.pid)
-                children = parent.children()
-                pid = children[-1].pid
+    for app_server in (PYVOY, GRANIAN, GUNICORN, HYPERCORN, UVICORN):
+        for interface in ("asgi", "wsgi"):
+            if not sys._is_gil_enabled() and app_server == GRANIAN:  # noqa: SLF001
+                # Granian hangs on free-threaded for some reason
+                continue
+            match interface:
+                case "asgi":
+                    if app_server.asgi_args is None:
+                        continue
+                    more_args = app_server.asgi_args
+                case "wsgi":
+                    if app_server.wsgi_args is None:
+                        continue
+                    more_args = app_server.wsgi_args
+            with subprocess.Popen(  # noqa: S603
+                [*app_server.args, *more_args],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            ) as server:
+                # Wait for server to start
+                started = False
+                for _ in range(100):
+                    try:
+                        with urllib.request.urlopen(
+                            "http://localhost:8000/controlled"
+                        ) as resp:
+                            if resp.status == 200:
+                                started = True
+                                break
+                    except Exception:  # noqa: S110
+                        pass
+                    time.sleep(0.1)
+                if server.returncode is not None or not started:
+                    server.terminate()
+                    stdout, stderr = server.communicate()
+                    msg = f"Server {app_server.name} failed to start\n{stderr.decode()}\n{stdout.decode()}"
+                    raise RuntimeError(msg)
 
-            monitor = ResourceMonitor(pid)
-            monitor.start()
+                pid = server.pid
+                if app_server in (GRANIAN, HYPERCORN, PYVOY):
+                    # find the worker process
+                    parent = psutil.Process(server.pid)
+                    children = parent.children()
+                    pid = children[-1].pid
 
-            for protocol in (Protocol.HTTP2, Protocol.HTTP1):
-                if protocol != Protocol.HTTP1 and app_server == UVICORN:
-                    continue
-                for sleep in (0, 10, 200, 500, 1000):
-                    for response_size in (0, 100, 10000, 100000):
-                        if args.short and (sleep > 0 or response_size > 0):
-                            continue
-                        print(  # noqa: T201
-                            f"Running benchmark for {app_server.name} with protocol={protocol.value} sleep={sleep}ms response_size={response_size}\n",
-                            flush=True,
-                        )
-                        target = {
-                            "method": "GET",
-                            "url": "http://localhost:8000/controlled",
-                            "header": {
-                                "X-Sleep-Ms": [str(sleep)],
-                                "X-Response-Bytes": [str(response_size)],
-                            },
-                        }
-                        vegeta_args = [
-                            "go",
-                            "run",
-                            "github.com/tsenart/vegeta/v12@v12.12.0",
-                            "attack",
-                            "-format=json",
-                            "-rate=0",
-                            "-max-workers=30",
-                            "-duration=5s",
-                        ]
-                        if protocol == Protocol.HTTP2:
-                            vegeta_args.append("-h2c")
+                monitor = ResourceMonitor(pid)
+                monitor.start()
 
-                        monitor.clear()
-                        vegeta = subprocess.Popen(  # noqa: S603
-                            vegeta_args,
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                            stderr=sys.stderr,
-                        )
-
-                        vegeta_result, _ = vegeta.communicate(
-                            f"{json.dumps(target)}\n".encode()
-                        )
-
-                        resource = monitor.aggregate()
-
-                        subprocess.run(
-                            [  # noqa: S607
+                for protocol in (Protocol.HTTP2, Protocol.HTTP1):
+                    if protocol != Protocol.HTTP1 and app_server == UVICORN:
+                        continue
+                    for sleep in (0, 10, 200, 500, 1000):
+                        for response_size in (0, 100, 10000, 100000):
+                            if args.short and (sleep > 0 or response_size > 0):
+                                continue
+                            print(  # noqa: T201
+                                f"Running benchmark for {app_server.name} with interface={interface} protocol={protocol.value} sleep={sleep}ms response_size={response_size}\n",
+                                flush=True,
+                            )
+                            target = {
+                                "method": "GET",
+                                "url": "http://localhost:8000/controlled",
+                                "header": {
+                                    "X-Sleep-Ms": [str(sleep)],
+                                    "X-Response-Bytes": [str(response_size)],
+                                },
+                            }
+                            vegeta_args = [
                                 "go",
                                 "run",
                                 "github.com/tsenart/vegeta/v12@v12.12.0",
-                                "report",
-                            ],
-                            check=True,
-                            input=vegeta_result,
-                        )
+                                "attack",
+                                "-format=json",
+                                "-rate=0",
+                                "-max-workers=30",
+                                "-duration=5s",
+                            ]
+                            if protocol == Protocol.HTTP2:
+                                vegeta_args.append("-h2c")
 
-                        print(  # noqa: T201
-                            f"\nCPU Percentage\t[Min, Max, Avg]\t\t{resource.min.cpu_percent:.2f}, {resource.max.cpu_percent:.2f}, {resource.avg.cpu_percent:.2f}%"
-                        )
-                        print(  # noqa: T201
-                            f"Memory RSS\t[Min, Max, Avg]\t\t{resource.min.rss}, {resource.max.rss}, {resource.avg.rss}\n"
-                        )
+                            monitor.clear()
+                            vegeta = subprocess.Popen(  # noqa: S603
+                                vegeta_args,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=sys.stderr,
+                            )
 
-                        print("\n", flush=True)  # noqa: T201
-            monitor.stop()
-            server.terminate()
-            server.communicate()
+                            vegeta_result, _ = vegeta.communicate(
+                                f"{json.dumps(target)}\n".encode()
+                            )
+
+                            resource = monitor.aggregate()
+
+                            subprocess.run(
+                                [  # noqa: S607
+                                    "go",
+                                    "run",
+                                    "github.com/tsenart/vegeta/v12@v12.12.0",
+                                    "report",
+                                ],
+                                check=True,
+                                input=vegeta_result,
+                            )
+
+                            print(  # noqa: T201
+                                f"\nCPU Percentage\t[Min, Max, Avg]\t\t{resource.min.cpu_percent:.2f}, {resource.max.cpu_percent:.2f}, {resource.avg.cpu_percent:.2f}%"
+                            )
+                            print(  # noqa: T201
+                                f"Memory RSS\t[Min, Max, Avg]\t\t{resource.min.rss}, {resource.max.rss}, {resource.avg.rss}\n"
+                            )
+
+                            print("\n", flush=True)  # noqa: T201
+                monitor.stop()
+                server.terminate()
+                server.communicate()
 
 
 if __name__ == "__main__":
