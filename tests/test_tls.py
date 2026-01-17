97623e855b71d2c8500c64bb4b471f990e98b540
Choko <choko@curioswitch.org>
2026-01-17 23:11:16 +0900

    Expose quic listener port (#178)

--- tests/test_tls.py
+++ tests/test_tls.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 import pytest
 import pytest_asyncio
 import trustme
-from pyqwest import Client, HTTPTransport
+from pyqwest import Client, HTTPTransport, HTTPVersion
 
 from pyvoy import Interface, PyvoyServer
 
@@ -38,6 +38,8 @@ def certs() -> Certs:
 async def server_asgi(certs: Certs) -> AsyncIterator[PyvoyServer]:
     async with PyvoyServer(
         "tests.apps.asgi.kitchensink",
+        port=0,
+        tls_port=0,
         tls_key=certs.server_key,
         tls_cert=certs.server_cert,
         stderr=subprocess.STDOUT,
@@ -51,6 +53,8 @@ async def server_wsgi(certs: Certs) -> AsyncIterator[PyvoyServer]:
     async with PyvoyServer(
         "tests.apps.wsgi.kitchensink",
         interface="wsgi",
+        port=0,
+        tls_port=0,
         tls_key=certs.server_key,
         tls_cert=certs.server_cert,
         stderr=subprocess.STDOUT,
@@ -73,27 +77,54 @@ def url(interface: Interface, url_asgi: str, url_wsgi: str) -> str:
             return url_wsgi
 
 
-@pytest_asyncio.fixture
-async def url_asgi(server_asgi: PyvoyServer) -> AsyncIterator[str]:
-    yield f"https://localhost:{server_asgi.listener_port}"
+@pytest.fixture(params=[HTTPVersion.HTTP1, HTTPVersion.HTTP2, HTTPVersion.HTTP3])
+def http_version(request: pytest.FixtureRequest) -> HTTPVersion:
+    return request.param
 
 
 @pytest_asyncio.fixture
-async def url_wsgi(server_wsgi: PyvoyServer) -> AsyncIterator[str]:
-    yield f"https://localhost:{server_wsgi.listener_port}"
+async def url_asgi(
+    server_asgi: PyvoyServer, http_version: HTTPVersion
+) -> AsyncIterator[str]:
+    yield f"https://localhost:{server_asgi.listener_port_tls if http_version != HTTPVersion.HTTP3 else server_asgi.listener_port_quic}"
 
 
 @pytest_asyncio.fixture
-async def client(certs: Certs) -> AsyncIterator[Client]:
+async def url_wsgi(
+    server_wsgi: PyvoyServer, http_version: HTTPVersion
+) -> AsyncIterator[str]:
+    yield f"https://localhost:{server_wsgi.listener_port_tls if http_version != HTTPVersion.HTTP3 else server_wsgi.listener_port_quic}"
+
+
+@pytest_asyncio.fixture
+async def client(certs: Certs, http_version: HTTPVersion) -> AsyncIterator[Client]:
     ssl_ctx = ssl.create_default_context()
     ssl_ctx.load_verify_locations(cadata=certs.ca.decode())
-    async with HTTPTransport(tls_ca_cert=certs.ca) as transport:
+    async with HTTPTransport(
+        tls_ca_cert=certs.ca, http_version=http_version
+    ) as transport:
         yield Client(transport)
 
 
 @pytest.mark.asyncio
-async def test_scope_content(url: str, client: Client) -> None:
+async def test_scope_content(
+    url: str, client: Client, http_version: HTTPVersion, interface: Interface
+) -> None:
     response = await client.get(f"{url}/echo-scope")
     assert response.status == 200, response.text()
     assert response.headers.get("x-scope-tls-version") == str(0x0304)
     assert response.headers.get("x-scope-tls-client-cert-name") == ""
+    match http_version, interface:
+        case HTTPVersion.HTTP3, "asgi":
+            expected = "3"
+        case HTTPVersion.HTTP2, "asgi":
+            expected = "2"
+        case HTTPVersion.HTTP1, "asgi":
+            expected = "1.1"
+        case HTTPVersion.HTTP3, "wsgi":
+            expected = "HTTP/3"
+        case HTTPVersion.HTTP2, "wsgi":
+            expected = "HTTP/2"
+        case HTTPVersion.HTTP1, "wsgi":
+            expected = "HTTP/1.1"
+    assert response.headers["x-scope-http-version"] == expected
